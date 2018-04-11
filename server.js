fdc79037c2da16555d241e551403ad49f29b291a
Paul Patterson <pddpatterson@gmail.com>
2018-04-11 23:09:18 +0200

    adds parcel build tool, surge for static publish of site, express static file serving

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const db = new Database();
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + '/dist/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 });
 
 app.post('/games/post', (req, res) => {
