90a4f2884bb5bdf5f216f998a9863e977d8305a0
Paul Patterson (WLT ZA) <ppatterson@tagworldwide.com>
2018-04-12 14:13:01 +0200

    adds bootsrap, jquery, skeleton modal components and configuration to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import API from './controllers/api';
+import GamesModal from '/components/Modals/GamesModal';
+import LeaderboardModal from '/components/Modals/LeaderboardModal';
 
 class App extends React.Component {
     constructor() {
         super()
+
+        this.state = {
+            title: "Rock Paper Scissors",
+            modals: {
+                games: {
+                    heading: "Recent Games",
+                    modalName: "gamesModal",
+                    data: {
+                        headers: [],
+                        dataRows: []
+                    }
+                },
+                leaderboard: {
+                    heading: "Leaderboard",
+                    modalName: "leaderModal",
+                    data: {
+                        headers: [],
+                        dataRows: []
+                    }
+                }
+            }
+        }
     };
+
+    componentDidMount() {
+        // Fetch Leader and Games Data here
+        // this.setState({ modals: { games: { data: API.getLeaderboard()}}});
+    }
 
     render() {
         return (
             <div>
-                <h1>We're in!</h1>    
+                <Header modalLinks={[this.state.modals.games.modalName, this.state.modals.leaderboard.modalName]} 
+                    title={this.state.title} 
+                />
+
+                <LeaderboardModal {...this.state.modals.games} />
+                <GamesModal {...this.state.modals.leaderboard} />
             </div>
         );
     };
